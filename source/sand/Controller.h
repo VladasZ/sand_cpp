f0e3361cf6ba4127c5ca50799dff7b51589b2dee
Vladas <146100@gmail.com>
2019-06-19 16:16:42 +0300

    ios tests

---
+++ source/sand/Controller.h
@@ -0,0 +1,16 @@
+//
+//  Controller.h
+//  cpp_utils
+//
+//  Created by Vladas Zakrevskis on 19/06/2019.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Controller : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
